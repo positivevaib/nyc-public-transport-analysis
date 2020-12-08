e94ff5370cd40c7a47dbfa8d407ff923c4fbfc46
Kristin Liu <jl11257@nyu.edu>
2020-12-08 05:13:48 -0500

    add subway before 10/11/2014 data first and second steps clean code

---
+++ core/cleaning/subway/cleanbymapreduce/clean_before_10_11_2014_data/second_step_clean/CleanMapper.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.util.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class CleanMapper extends Mapper<LongWritable, Text, Text, Text> {
+
+    final static String DATE_FORMAT = "MM-dd-yyyy";
+
+    public static boolean isDateValid(String date) 
+    {
+        try {
+            DateFormat df = new SimpleDateFormat(DATE_FORMAT);
+            df.setLenient(false);
+            df.parse(date);
+            return true;
+        } catch (ParseException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String[] line = value.toString().split(",");
+
+        if (line.length < 8) {
+            return;
+        }
+
+        String controlArea = line[0].trim();
+        if ("".equals(controlArea)) {
+            return;  
+        }   
+
+        String remoteUnit = line[1].trim();
+        if ("".equals(remoteUnit)) {
+            return;  
+        }   
+
+        String subunitChannelPosition = line[2].trim();
+        if ("".equals(subunitChannelPosition)) {
+            return; 
+        }    
+
+        if (!isDateValid(line[3])) {
+            return;
+        }
+
+        int year = Integer.parseInt(line[3].split("-")[2]); 
+        int month = Integer.parseInt(line[3].split("-")[0]);
+        int day = Integer.parseInt(line[3].split("-")[1]);
+
+        int hour = Integer.parseInt(line[4].split(":")[0]);
+        if (hour < 0 || hour > 23) {
+            return; 
+        }
+
+        String description = line[5].trim();
+        if (description.matches("REGULAR") == false) {
+            return;           
+        }
+
+        int entries = Integer.parseInt(line[6].trim());
+        if (entries < 0){
+            return; 
+        }   
+
+        int exits = Integer.parseInt(line[7].trim());
+        if (exits < 0){
+            return; 
+        }   
+        
+        context.write(new Text(remoteUnit + ""), new Text(controlArea + "," + remoteUnit + "," + subunitChannelPosition + "," 
+            + year + "," + month + "," + day + "," + hour + "," + entries + "," + exits)); 
+    }
+}
