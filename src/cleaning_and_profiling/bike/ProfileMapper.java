fada1c4ffb9579b90013820e74dc27b6d969abfa
Vaib Gadodia <positivevaib@users.noreply.github.com>
2020-12-16 01:53:25 -0500

    Update bike cleaning code and create new directory for final submission

---
+++ src/cleaning_and_profiling/bike/ProfileMapper.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class ProfileMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        context.write(new Text("rawCount"), new IntWritable(1));
+    }
+}
+
